50fb218429a458149b2027559f09d3a9b563bb5e
Christopher Burke <christopherjamesburke@gmail.com>
2018-03-13 16:37:50 -0400

    Updated code to look at nested folders. New README.md file.

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ These are python scripts I have created, modified or used. Some scripts are for 
 
 | Script  | DocString |
 | ------------- | ------------- |
+|ad/account_expiration.py|Account Expiration|
 |default_pw_generator.py|Default initial password generator.|
 |delta_date.py|Return the delta of the current date.|
 |dev_utils.py|Collection of Utilities.|
@@ -14,6 +15,7 @@ These are python scripts I have created, modified or used. Some scripts are for 
 |find_files.py|Find all files matching a pattern.|
 |find_local_ips.py|Find all local ip addresses in an ip network.|
 |FizzBuzz.py|FizzBuzz Test.|
+|ifttt/ifttt_event_trigger.py|IFTTT Maker Webhooks event trigger.|
 |inquisitor.py|List file type info.|
 |iTunes.py|Python Script to get the Track, Artist and Album from iTunes. |
 |mta_service.py|Python Script to get the status of MTA Service.|
@@ -23,8 +25,5 @@ These are python scripts I have created, modified or used. Some scripts are for 
 |random_password_gen.py|Class based random password generator.  Uses string, random, sys modules.|
 |readme.py|Generate the README.md file a repo.|
 |remove_pyc.py|Search and remove pyc files.|
-|reverse.py|Reverse an input string.|
 |SuperBowlNumbers.py|Get the numbers for the superbowl box pools.|
-|watch_dir_windows.py|Watch Directory Windows.|
-|watch_windows.py|Watch Windows.|
 |weather.py|Weather from weather.gov.|
--- readme.py
+++ readme.py
@@ -5,11 +5,17 @@
 from pathlib import Path
 import ast
 from jinja2 import Environment, FileSystemLoader
-
+from subprocess import Popen, PIPE
 
 file_loader = FileSystemLoader('templates')
 env = Environment(loader=file_loader)
 template = env.get_template('readme_template.md')
+
+FILES = Popen("git ls-tree -r master --name-only",
+              shell=True, stdout=PIPE).stdout.read()
+FILES = FILES.decode().split('\n')
+
+scripts = []
 
 
 def get_value(value):
@@ -20,24 +26,43 @@ def get_value(value):
         return None
 
 
+def process(path, name):
+    d = {}
+    global scripts
+    with open(path.as_posix()) as fd:
+        file_contents = fd.read()
+    module = ast.parse(file_contents)
+    docstring = ast.get_docstring(module)
+    docstring_line = get_value(docstring)
+    if docstring_line:
+        d['name'] = name
+        d['docstring'] = docstring_line
+        scripts.append(d)
+
+
+def search_dir(d, basepath, extension):
+    """Search directories recursively for repo files."""
+    for path in d.iterdir():
+        string_path = str(path).replace(basepath, '')[1:]
+        if path.is_dir():
+            search_dir(path, basepath)
+        if string_path in FILES and path.suffix == extension:
+            print(string_path)
+            process(path, string_path)
+
+
+def write_readme():
+    """Write readme.md file."""
+    global scripts
+    with open('README.md', 'w') as f:
+        f.write(template.render(scripts=scripts))
+
+
 def main():
     """Create a readme.md file."""
-    with open('README.md', 'w') as f:
-        p = Path.cwd()
-        scripts = []
-        for path in p.iterdir():
-            d = {}
-            if path.suffix == '.py':
-                with open(path.as_posix()) as fd:
-                    file_contents = fd.read()
-                module = ast.parse(file_contents)
-                docstring = ast.get_docstring(module)
-                docstring_line = get_value(docstring)
-                if docstring_line:
-                    d['name'] = str(path.name)
-                    d['docstring'] = docstring_line
-                    scripts.append(d)
-        f.write(template.render(scripts=scripts))
+    p = Path.cwd()
+    search_dir(p, str(p))
+    write_readme()
 
 
 if __name__ == "__main__":
