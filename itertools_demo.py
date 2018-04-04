a591e0da934c35e55b77f93485ae4ca5d9059770
Christopher Burke <christopherjamesburke@gmail.com>
2018-04-04 13:40:18 -0400

    Added Product and Repeat wrapper functions.

--- itertools_demo.py
+++ itertools_demo.py
@@ -2,7 +2,7 @@
 
 """Itertools demo."""
 
-from itertools import permutations, combinations
+from itertools import permutations, combinations, product, repeat
 
 
 def p(iterable, length=2):
@@ -15,8 +15,20 @@ def c(iterable, length=2):
     return combinations(iterable, r=length)
 
 
+def prod(item, iterable, length=2):
+    """A Product wrapper function."""
+    return product([item], iterable)
+
+
+def r(item, times=2):
+    """A Repeat wrapper function."""
+    return repeat(object=item, times=times)
+
+
 if __name__ == "__main__":
     t = (1, 2, 3, 4, 5, 6,)
     print(t)
     print(list(p(t, 2)))
     print(list(c(t, 2)))
+    print(list(prod(t[0], t, 2)))
+    print(list(r(t, 10)))
