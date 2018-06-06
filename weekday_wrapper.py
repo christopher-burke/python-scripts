f4062ec56e216715e5b7b58ddafdad4b64f05e13
Christopher Burke <christopherjamesburke@gmail.com>
2018-06-06 11:37:04 -0400

    Simple weekday wrapper. Week number and Weekday functions.

---
+++ weekday_wrapper.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+"""Useful Weekday wrappers."""
+
+
+from datetime import date
+from calendar import day_name
+
+
+def week_num():
+    """Return the current week number."""
+    return date.today().isocalendar()[1]
+
+
+def weekday_num():
+    """Return the current weekday number.
+
+    Using the Python datetime: 0 is Sunday, 6 is Saturday.
+    """
+    weekday_num = date.today().isocalendar()[2]
+    return weekday_num
+
+
+def weekday():
+    """Return the currecnt weekday name."""
+    weekday_num = date.today().isocalendar()[2]
+    return day_name[weekday_num]
+
+
+if __name__ == "__main__":
+    print(week_num())
