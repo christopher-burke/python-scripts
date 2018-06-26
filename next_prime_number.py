bc1b99b0bd5d0bbcc082fc07f5addb74a5f92d99
Christopher Burke <christopherjamesburke@gmail.com>
2018-06-26 10:13:05 -0400

    Updated. Added cmd line args and prompt. Also random int selection.

--- next_prime_number.py
+++ next_prime_number.py
@@ -3,7 +3,9 @@
 
 """Next Prime Number."""
 
-from math import sqrt, ceil
+import random
+import sys
+from math import sqrt
 from functools import reduce
 
 
@@ -12,17 +14,38 @@ def factors(n):
 
     https://stackoverflow.com/questions/6800193/what-is-the-most-efficient-way-of-finding-all-the-factors-of-a-number-in-python
     """
-    return set(reduce(list.__add__,
-                      ([i, n//i] for i in range(1, int(sqrt(n) + 1)) if n % i == 0)))
+    n_factors = ([i, n//i] for i in range(1, int(sqrt(n) + 1)) if n % i == 0)
+    return set(reduce(list.__add__, n_factors))
 
 
-def is_prime(x):
+def prime(n):
     """Return True if x is prime, False otherwise."""
-    if len(factors(x)) == 2:
+    if n % 2 == 0:
+        return False
+    if len(factors(n)) == 2:
         return True
     else:
         return False
 
 
+def next_prime(n):
+    """Return the next prime number after n."""
+    while True:
+        n = n + 1
+        if prime(n):
+            return n
+
+
 if __name__ == "__main__":
-    print(is_prime(337))
+    try:
+        n = sys.argv[1]
+    except IndexError:
+        n = input('Enter an integer > 1: ')
+
+    try:
+        n = int(n)
+    except ValueError:
+        n = random.randint(2, 2**56)
+        print(f'{n} is being used.')
+
+    print(f'The next prime number greater than {n} is {next_prime(n)}.')
