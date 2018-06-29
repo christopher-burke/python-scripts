b0b8d559680be5cc09c68d29a88e872e870ff375
Christopher Burke <christopherjamesburke@gmail.com>
2018-06-29 14:36:23 -0400

    Added more tests to __main__. Updated main().

--- ad/account_expiration.py
+++ ad/account_expiration.py
@@ -13,6 +13,8 @@ The date when the account expires.
 This value represents the number of 100-nanosecond intervals since January 1, 1601 (UTC).
 A value of 0 or 0x7FFFFFFFFFFFFFFF (9223372036854775807) indicates that the account never expires.
 
+
+# v / 864000000000.0 - 109207)
 """
 
 from datetime import datetime as dt
@@ -28,12 +30,18 @@ def calculate_nanoseconds(from_, to_):
     return delta.total_seconds() / 0.0000001
 
 
-def main():
-    now = dt.now()
-    value = calculate_nanoseconds(from_=ACCOUNT_EXPIRATION_DATE, to_=now)
+def main(nanos=None):
+    if not nanos:
+        nanos = now
+    value = calculate_nanoseconds(from_=ACCOUNT_EXPIRATION_DATE, to_=nanos)
     value = math.floor(value)
     print(f'{value:f}'.split('.')[0])
 
 
 if __name__ == "__main__":
     main()
+    main(nanos=dt(2018, 5, 18, 23, 0, 0))  # 131711760000000000
+    main(nanos=dt(2018, 5, 31, 23, 0, 0))  # 131722992000000000
+    main(nanos=dt(2017, 12, 23, 0, 0, 0))  # 131584788000000000
+    # print(131711760000000000 - 131711580000000000)
+# 2018 - 05 - 18 23: 00: 00.0000000
