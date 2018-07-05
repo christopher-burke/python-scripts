8a14899b4ce1d3d496bc373da796f7b28dbb4af5
Christopher Burke <christopherjamesburke@gmail.com>
2018-07-05 13:00:51 -0400

    Removed comments from code.

--- breathe.py
+++ breathe.py
@@ -14,14 +14,12 @@ def breathe(inhale_time: int=4, exhale_time: int=6):
     """
     for _ in range(0, inhale_time+1):
         sys.stdout.write('\r')
-        # sys.stdout.write("[%-10s] %d%%" % ('='*(_*in_meter), (in_meter*_)))
         sys.stdout.write("[BREATH IN ] {}".format(_))
         sys.stdout.flush()
         sleep(1)
 
     for _ in reversed(range(0, exhale_time+1)):
         sys.stdout.write('\r')
-        # sys.stdout.write("[%-10s] %d%%" % ('='*_, (out_meter*_)))
         sys.stdout.write("[BREATH OUT] {}".format(_))
         sys.stdout.flush()
         sleep(1)
