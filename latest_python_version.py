6cf208b1de7dcfbf37ce1c42e3b8dcffe2507180
Christopher Burke <christopherjamesburke@gmail.com>
2018-08-16 15:32:17 -0400

    Updated with asyncio module.

--- latest_python_version.py
+++ latest_python_version.py
@@ -5,29 +5,61 @@
 import requests
 import re
 from bs4 import BeautifulSoup
+import asyncio
+from functools import partial
 
 
-def parse(content):
-    """Find the Download Widet class."""
-    soup = BeautifulSoup(content, "html.parser")
-    dw = soup.select('div.download-widget')
-    return dw[-1].text
-
-
-def latest(url):
-    """Find the latest python version number."""
+async def fetch(url):
+    """Async function to get URL."""
     r = requests.get(url)
-    result = parse(r.content)
-    py_version = re.findall(r'Python ([\d,\.]+)+', result, re.I)
+    return r
+
+
+def regex(pattern):
+    """Return compiled regular expression pattern."""
+    return re.compile(pattern)
+
+
+def parse(content, div_select):
+    """Find the Download Widget class."""
+    soup = BeautifulSoup(content, "html.parser")
+    dw = soup.select(div_select)
+    return dw
+
+
+async def pparse(div_select):
+    """Build parser partially."""
+    return partial(parse, div_select=div_select)
+
+
+async def handler(url, pattern, div_select):
+    """Asyncio handler."""
+    py_website = asyncio.ensure_future(fetch(url=url))
+    parser = asyncio.ensure_future(pparse(div_select=div_select))
+    py_version_regex = regex(pattern=pattern)
+    await asyncio.wait([py_website])
+    py_website_source = py_website.result().content
+    await asyncio.wait([parser])
+    py_latest_div_parser = parser.result()
+    result = py_latest_div_parser(content=py_website_source)
+    py_version = py_version_regex.findall(str(result), re.I)
+
     return py_version[-1]
 
 
-def main(url):
-    """Return the version number."""
-    c = latest(url)
-    print(c)
+def main(url, pattern, div_select):
+    """Print the version number."""
+    loop = asyncio.get_event_loop()
+    c = loop.run_until_complete(asyncio.gather(
+        handler(url=url,
+                pattern=pattern,
+                div_select=div_select)))
+
+    print(*c)
 
 
 if __name__ == "__main__":
     url = 'https://python.org'
-    main(url=url)
+    pattern = r'(?<=Latest: )<a[\w\W\s\S]+(?<=Python )([\d\.]+)(?=</a></p>)'
+    div_select = 'div.download-widget'
+    main(url=url, pattern=pattern, div_select=div_select)
