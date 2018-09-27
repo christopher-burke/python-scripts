cc596d1c53a1cd18f7abb0445c5e525e15beab1d
Christopher Burke <christopherjamesburke@gmail.com>
2018-09-27 16:26:56 -0400

    Updated with f-strings.

--- nyj_live_score.py
+++ nyj_live_score.py
@@ -5,29 +5,38 @@
 from urllib.request import urlopen
 from xml.etree import ElementTree
 
-# Season Scores
+
 url_2 = 'http://www.nfl.com/ajax/scorestrip?season=%d&seasonType=REG&week=%d'
 
 
 def main(year):
     """Return the Jets scores for a given year (season)."""
-    for y in [year, ]:
-        for i in range(1, 18):
-            f = urlopen(url_2 % (y, i))
-            tree = ElementTree.parse(f)
-            f.close()
-            for node in tree.iter('g'):
-                date = node.attrib.get('eid')
-                weekday = node.attrib.get('d')
-                time = node.attrib.get('t')
-                h = node.attrib.get('h')
-                v = node.attrib.get('v')
-                hs = node.attrib.get('hs')
-                vs = node.attrib.get('vs')
-                if (v) == 'NYJ'or (h) == 'NYJ':
-                    print(v, vs, h, hs, weekday, "%s/%s/%s" %
-                          (date[4:6], date[6:8], date[:4]), time)
+    for week in range(1, 18):
+        url = f'http://www.nfl.com/ajax/scorestrip?season={year}&seasonType=REG&week={week}'
+        results = urlopen(url)
+        tree = ElementTree.parse(results)
+        results.close()
+        for node in tree.iter('g'):
+            date = node.attrib.get('eid')
+            weekday = node.attrib.get('d')
+            time = node.attrib.get('t')
+            home_team = node.attrib.get('h')
+            visitor_team = node.attrib.get('v')
+            home_score = node.attrib.get('hs')
+            visitor_score = node.attrib.get('vs')
+            if 'NYJ' in (visitor_team, home_team):
+                print(
+                    f'{home_team} '
+                    f'{home_score} '
+                    f'{visitor_team} '
+                    f'{visitor_score} '
+                    f'{weekday} '
+                    f'{date[4:6]} '
+                    f'{date[6:8]} '
+                    f'{date[:4]} '
+                    f'{time}'
+                )
 
 
 if __name__ == "__main__":
-    main(2017)
+    main(2018)
