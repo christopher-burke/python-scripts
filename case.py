6976bacb594dbade5073834408dada148f9566bb
Christopher Burke <christopherjamesburke@gmail.com>
2018-10-29 12:27:05 -0400

    Updated case code. Handles spaces and  Camel case to snake case.

--- case.py
+++ case.py
@@ -10,20 +10,29 @@ and converts them to snake case (i.e. this_is_camel_cased).
 import re
 
 
-def snake_case(input_str: str) -> str:
+def snake_case(input_str: str, camel_case=False) -> str:
     """
     Turn camel case into snake case.
 
     :param input_str: String of variable.
     :return: Snake case string of input_str.
     """
-    regex = r"([A-Z])"
 
-    snake_case = input_str[0].lower()+re.sub(regex, r'_\1',
-                                             input_str[1:]).lower()
+    if camel_case:
+        regex = r"([A-Z])"
+        substitution = r'_\1'
+    else:
+        regex = r"([\s])"
+        substitution = r'_'
+
+    snake_case = input_str[0].lower() +\
+        re.sub(regex,
+               substitution,
+               input_str[1:]).lower()
 
     return f'{snake_case}'
 
 
 if __name__ == "__main__":
-    print(snake_case('ThisIsCamelCased'))
+    print(snake_case('ThisIsCamelCased', camel_case=True))
+    print(snake_case('This includes spaces'))
