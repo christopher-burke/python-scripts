e7998faeacd77dfcc7d14f9dacbdadedb71c83cf
Christopher Burke <christopherjamesburke@gmail.com>
2018-11-01 16:41:34 -0400

    Starting to code Clipboard History script.

---
+++ clipboard_history.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""Clipboard History.
+
+Create a file that logs the text copied to the clipboard.
+"""
+
+
+def main():
+    """Save copied text to clipboard."""
+    raise NotImplementedError("Have not started yet.")
+
+
+if __name__ == "__main__":
+    main()
