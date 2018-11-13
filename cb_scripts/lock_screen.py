e17a884c582d75c723bf5282bc2e35376f3bc3fc
Christopher Burke <christopherjamesburke@gmail.com>
2018-11-13 09:51:59 -0500

    Updated for different platforms.

--- cb_scripts/lock_screen.py
+++ cb_scripts/lock_screen.py
@@ -3,15 +3,33 @@
 """Lock the Screen."""
 
 from subprocess import Popen, PIPE
+from platform import system
+
+
+os_lock_commands = {
+    'darwin':
+        ['/System/Library/CoreServices/Menu Extras/User.menu/Contents/Resources/CGSession',
+         '-suspend',
+         ],
+    'linux':
+        ['gnome-screensaver-command',
+         '--lock',
+         ],
+}
 
 
 def lock_screen():
-    """Lock the screen. For macOS."""
-    command = r'/System/Library/CoreServices' \
-        r'/Menu\ Extras/User.menu' \
-        r'/Contents/Resources/CGSession' \
-        r' -suspend'
-    Popen(command, shell=True, stdout=PIPE)
+    """Lock the screen.
+
+    For macOS, Linux or Windows operating systems.
+    """
+    os_system = system().lower()
+    if os_system == 'windows':
+        import ctypes
+        ctypes.windll.user32.LockWorkStation()
+    else:
+        command = os_lock_commands[os_system]
+        Popen(command, shell=False, stdout=PIPE)
 
 
 if __name__ == "__main__":
