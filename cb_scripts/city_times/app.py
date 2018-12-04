7773e38b33b13d327ad45756db9c566e1b0023c4
Christopher Burke <christopherjamesburke@gmail.com>
2018-12-04 17:00:13 -0500

    Updated index view.

--- cb_scripts/city_times/app.py
+++ cb_scripts/city_times/app.py
@@ -2,15 +2,28 @@
 
 
 from flask import Flask, render_template, request
+from collections import namedtuple
 
 app = Flask(__name__)
+
+
+CityTime = namedtuple('CityTime', ['name', 'time'])
 
 
 @app.route('/')
 @app.route('/', methods=['POST'])
 def index():
     """Index method."""
-    return None
+    data = []
+    if request.method == 'POST':
+        city_a = request.form.get('city-a')
+        city_b = request.form.get('city-b')
+
+        for city in (city_a, city_b,):
+            time = 1  # TODO: Add function to look up time.
+            data.append(CityTime(name=city, time=time))
+
+    return render_template("time.html", data=data)
 
 
 if __name__ == "__main__":
