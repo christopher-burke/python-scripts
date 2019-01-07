1045a7362879f4664d43542611d48e0f4232a8fa
Christopher Burke <christopherjamesburke@gmail.com>
2019-01-07 16:37:22 -0500

    Added command. Updated print statements.

--- cb_scripts/useful_commands.py
+++ cb_scripts/useful_commands.py
@@ -5,22 +5,21 @@
 
 def main():
     """Print useful commands."""
-    print("""
-    # Update Python in Virtual Envs need to automate this part
-    # echo "run lsvirtualenv -b | xargs -I % sh -c 'find ~/.virtualenvs/% -type l -delete; virtualenv ~/.virtualenvs/%'"
-    find ~/.virtualenvs ! -path ~/.virtualenvs -maxdepth 1 -type d | xargs -I % sh -c 'find % -type l -delete;virtualenv %'
 
     # Python
-    ## virtualenvs
-    echo "and pip3 freeze --local | grep -v ‘^\-e’ | cut -d = -f 1 | xargs pip3 install -U"
-    #lsvirtualenv -b | xargs -I % sh -c 'source /Users/wheeljack/.virtualenvs/%/bin/activate;pip3 freeze --local | grep -v ‘^\-e’ | cut -d = -f 1 | xargs pip3 install -U'
+    # Commands for virtualenvs:
+    # Update Python in Virtual Envs need to automate this part.
+    print(r"""find ~/.virtualenvs ! -path ~/.virtualenvs -maxdepth 1 -type d | xargs -I % sh -c 'find % -type l -delete;virtualenv %'""")
 
-    ### Update packages for each virtualenv
-    ls -d ~/.virtualenvs/*/ | xargs -I % sh -c 'source %/bin/activate;pip3 freeze --local | grep -v ‘^\-e’ | cut -d = -f 1 | xargs pip3 install -U;deactivate'
+    # Update packages for each virtualenv.
+    print(r"""ls -d ~/.virtualenvs/*/ | xargs -I % sh -c 'source %/bin/activate;pip3 freeze --local | grep -v ‘^\-e’ | cut -d = -f 1 | xargs pip3 install -U;deactivate'""")
 
-    ### create requirements.txt for each virtualenv
-    ls -d ~/.virtualenvs/*/ | xargs -I % sh -c 'source %/bin/activate;pip3 freeze --local > %requirements.txt; deactivate'
-    """)
+    # create requirements.txt for each virtualenv.
+    print(r"""ls -d ~/.virtualenvs/*/ | xargs -I % sh -c 'source %/bin/activate;pip3 freeze --local > %requirements.txt; deactivate'""")
+
+    # Ubuntu
+    # Blank screen on Ubunutu.
+    print(r"sudo sh -c 'vbetool dpms off;read ans; vbetool dpms on'")
 
 
 if __name__ == "__main__":
