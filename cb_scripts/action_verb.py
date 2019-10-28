9121b0e0e5e07876a98ce28618318256d162a2f0
Christopher Burke <christopherjamesburke@gmail.com>
2019-10-28 15:08:28 -0400

    Added action_verb script.

---
+++ cb_scripts/action_verb.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+"""Action Verbs.
+
+Pick a random action verb from data/action_verb.json.
+
+"""
+
+import random
+import json
+
+
+def load(
+        action_verb_json: str = 'cb_scripts/data/json/action_verbs.json',
+        key: str = 'ACTION_VERBS'
+):
+    """Load the action verbs."""
+    with open(action_verb_json, 'r') as raw_data:
+        data = json.load(raw_data)
+    return data['ACTION_VERBS']
+
+
+def main():
+    """Pick random action verb."""
+    return random.choice(load())
+
+
+action_verb = main
+
+if __name__ == "__main__":
+    print(main())
