f5dceca0bdbe9de6197b26858600b792f6adff8a
Christopher Burke <christopherjamesburke@gmail.com>
2022-04-25 16:31:33 -0400

    Improved script, version minor number sort.

--- cb_scripts/latest_python_FTP_version.py
+++ cb_scripts/latest_python_FTP_version.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
 """Get the latest stable version number of Python from FTP site.
-
 pip install requests beautifulsoup4
 """
 
 import re
+import sys
+from typing import Optional
 
 import requests
 from bs4 import BeautifulSoup
@@ -17,20 +18,28 @@ def parse(content):
     ftp_links = [a.text for a in soup.select('a')]
     regex = re.compile(r'(?<!.)(\d[\d.]+)')
     versions = filter(regex.search, ftp_links)
-    return list(versions)
+    versions = [tuple(map(int, re.sub("/", "", version).split('.')))
+                for version in versions]
+    return versions
 
 
-def latest(url):
+def latest(url: str) -> Optional[str]:
     """Find the latest python version number."""
     response = requests.get(url)
-    py_versions = sorted(parse(response.content))
-    return py_versions[-1][:-1]
+    if response.status_code == 200:
+        py_versions = sorted(parse(response.content))
+        return py_versions[-1]
+    return None
 
 
-def main(url):
+def main(url: str) -> None:
     """Return the version number."""
     version = latest(url)
-    print(version)
+    if version:
+        print(".".join(map(str, version)))
+    else:
+        print('Error')
+        sys.exit(1)
 
 
 if __name__ == "__main__":
